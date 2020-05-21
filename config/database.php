237b373f1ff4d1f2ff6b25307ef1c902433e34f4
bobocodex57 <bobo.codex@gmail.com>
2020-05-21 21:47:22 +0630

    database_file_update

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('db4free.net'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'laravel_social'),
+            'username' => env('DB_USERNAME', 'mgbobo'),
+            'password' => env('DB_PASSWORD', 'Htet*****541574'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
